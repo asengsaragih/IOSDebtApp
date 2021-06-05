ca28cc143c12f3f03ca646d1553dac7431ff7a33
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-06-05 15:54:15 +0700

    Initial Commit

---
+++ Debt/DebtApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebtApp.swift
+//  Debt
+//
+//  Created by Fiqry choerudin on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DebtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
